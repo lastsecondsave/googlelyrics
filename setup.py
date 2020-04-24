e917d323baad0748d4a09efe3e60b8b1de7a6468
lastsecondsave <lastsecondsave@outlook.com>
2020-04-24 21:18:06 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="googlelyrics",
+    version="0.1",
+    description="Grab lyrics from Google search results.",
+    author="lastsecondsave",
+    packages=["googlelyrics"],
+    python_requires=">=3.6",
+    install_requires=[
+        "requests>=2.22.0",
+        "beautifulsoup4>=4.9.0"
+    ],
+    entry_points={
+        "console_scripts": [
+            "googlelyrics = googlelyrics.__main__:main"
+        ]
+    }
+)
